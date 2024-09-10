eb6971df62bcc4e43e9ab8ac1f3c4003a52e7793
prasannak <119028680+prasannasalt@users.noreply.github.com>
2024-09-10 16:26:25 +0530

    Image Slider has Created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Accordian from "./components/accordian/index";
+import ImageSlider from "./components/image-slider";
+import RandomColor from "./components/random-color/index";
+import StartRating from "./components/start-rating/index";
+import Intro from "./components/intro";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Navigation Bar */}
+        <nav>
+          <ul>
+          <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/accordion">Accordion</Link>
+            </li>
+            <li>
+              <Link to="/random-color">Random Color</Link>
+            </li>
+            <li>
+              <Link to="/star-rating">Star Rating</Link>
+            </li>
+            <li>
+              <Link to="/image-slider">Image Slider</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+        <Route path="/" element={<Intro/>} />
+          <Route path="/accordion" element={<Accordian />} />
+          <Route path="/random-color" element={<RandomColor />} />
+          <Route path="/star-rating" element={<StartRating noOfStars={10} />} />
+          <Route
+            path="/image-slider"
+            element={
+              <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"} limit={"10"} />
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/intro.js
@@ -0,0 +1,24 @@
+// src/components/Intro.js
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <h1>Welcome to My Portfolio</h1>
+      <p>
+        Hi, I'm Prasanna Kulkarni, a Full Stack Developer. I
+        have over 2 years of experience in building web applications, focusing
+        on Node.js and Express. Currently, I am working on e-commerce projects,
+        developing REST APIs, managing databases, and ensuring seamless user
+        experiences.
+      </p>
+      <p>
+        I'm passionate about coding, learning new technologies, and solving
+        real-world problems through my skills. Take a look around to see some of
+        my work and projects.
+      </p>
+    </div>
+  );
+};
+
+export default Intro;
